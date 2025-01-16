ce34f7b12bd406d29d613fb34650685c30f9820a
Danilo91Colloca <danilo.colloca91@gmail.com>
2025-01-16 11:49:37 +0100

    added file to test json data manipulations- created get_movies/add_movie/__update_json_file methods

---
+++ movie_library.py
@@ -0,0 +1,49 @@
+import json
+"""production file json"""
+#file_json_path = "/Applications/MAMP/htdocs/programming-principle-scritto/movies.json"
+"""test file json"""
+file_json_path = "/Applications/MAMP/htdocs/programming-principle-scritto/test.json"
+
+    
+"""Define Movie library class"""
+class MovieLibrary:
+    """
+        A class rappresenting a library of movies with imports of all movies 
+        and a collection of a movie that has title,director,year,genre
+
+        Attributes: 
+            json_file:  
+            movies: collection of film
+    """
+    def __init__(self, json_file, movies):
+        self.json_file = json_file
+        self.movies = movies
+    
+    def get_movies(self):
+        return self.movies
+    
+    def __update_json_file(self, new_movie):
+        with open(self.json_file, "w", encoding="utf-8") as inside_movies:
+            return json.dump(new_movie, inside_movies, indent=4)
+
+    def add_movie(self, title:str, director:str, year:int, genres:list):
+        data = {
+            "title": title,
+            "director": director,
+            "year": year,
+            "genres": genres
+        }
+        self.movies.append(data)
+
+        self.__update_json_file(self.movies)
+
+
+""" define function that deserialize json """       
+def jsonDeserializer(json_to_deserialize):
+    with open(json_to_deserialize, "r") as all_movies:
+        return json.load(all_movies)
+    
+movies_list = jsonDeserializer(file_json_path)
+
+library = MovieLibrary(file_json_path, movies_list)
+library.add_movie("cane","danny", 1991, ["speriamo", "bene"])

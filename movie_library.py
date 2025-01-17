09cd053751551ab8886086fce2f693c4f4668112
Danilo91Colloca <danilo.colloca91@gmail.com>
2025-01-17 23:51:48 +0100

    added comment 18 tasks over 18

--- movie_library.py
+++ movie_library.py
@@ -113,7 +113,7 @@ class MovieLibrary:
             Args:
                 title (str): the new movie's title.
                 director (str): the new movie's director.
-                year (int): the new movie's publication year.
+                year (int): the new movie's released year.
                 genres (list): the new movie' s genres list.
 
         """
@@ -309,7 +309,18 @@ class MovieLibrary:
                 __movies_by_substring.append(self.movies[index_item])
         return __movies_by_substring
 
-    def get_movies_by_year(self, year: int):
+    def get_movies_by_year(self, year: int) -> list:
+        """
+            Get the movies by specific year.
+
+            Args:
+                year (str): The release's year of the movies.
+
+            Returns:
+                list: that contain all movies released in
+                that specific year.
+        """
+        
         __index = None
         __all_movies_by_year = []
         for index_item, key in enumerate(self.movies):
@@ -318,14 +329,36 @@ class MovieLibrary:
                 __all_movies_by_year.append(self.movies[__index])
         return __all_movies_by_year
 
-    def count_movies_by_director(self, director: str):
+    def count_movies_by_director(self, director: str) -> int:
+        """
+            Count movie made by a specific director.
+
+            Args:
+                director (str): The director's name.
+            
+            Return:
+                int: the number of the movies.
+
+        """
+
         __count_movies_by_director = 0
-        for i, key in enumerate(self.movies):
+        for idex_item, key in enumerate(self.movies):
             if key["director"].casefold() == director.casefold():
                 __count_movies_by_director += 1
         return __count_movies_by_director
 
-    def get_movies_by_genre(self, genre: str):
+    def get_movies_by_genre(self, genre: str) -> list:
+        """
+            Get movies by a specific genre.
+
+            Args:
+                genre (str): The name of genre
+
+            Return:
+                list: List of movies with the specific genre 
+
+        """
+
         __movies_by_genre = []
         for index_movie, key in enumerate(self.movies):
             for i in key["genres"]:
@@ -333,20 +366,44 @@ class MovieLibrary:
                     __movies_by_genre.append(self.movies[index_movie])
         return __movies_by_genre
 
-    def get_oldest_movie_title(self):
+    def get_oldest_movie_title(self) -> str:
+        """
+            Get oldest movie.
+
+            Return:
+                str: the title of oldest movie into the file movies
+        
+        """
+        
         __reorderd_movies_list = sorted(
             self.movies,
             key=lambda x: x['year']
         )
-        return __reorderd_movies_list[0]
+        return __reorderd_movies_list[0]["title"]
 
-    def get_average_release_year(self):
+    def get_average_release_year(self) -> float:
+        """
+            Get the average release date.
+
+            Return:
+                float: The result of average calculation.
+
+        """
+        
         __all_years_to_sum = []
         for index_item, key in enumerate(self.movies):
             __all_years_to_sum.append(float(key["year"]))
         return sum(__all_years_to_sum)/len(__all_years_to_sum)
 
-    def get_longest_title(self):
+    def get_longest_title(self) -> str:
+        """
+            Get the longest title into the file movies.
+
+            Return:
+                str: The longest title.
+
+        """
+        
         __longest_title = sorted(
             self.movies,
             key=lambda k: len(k["title"]),
@@ -354,14 +411,39 @@ class MovieLibrary:
         )
         return __longest_title[0]["title"]
 
-    def get_titles_betweens_years(self, start_year: int, end_year: int):
+    def get_titles_betweens_years(
+        self,
+        start_year: int,
+        end_year: int
+        ) -> list:
+        """
+            Get movie's titles that are released between two year 
+            (including start/end years).
+
+            Args:
+                start_year (int): start range year.
+                end_year (int): end range year. 
+
+            Return:
+                list: List of titles.
+
+        """
+
         __title_list_list = []
         for i, key in enumerate(self.movies):
             if key["year"] >= start_year and key["year"] <= end_year:
                 __title_list_list.append(key["title"])
         return __title_list_list
 
-    def get_most_common_year(self):
+
+    def get_most_common_year(self) -> int:
+        """
+            Get most common year.
+
+            return:
+                int: the year most commont among the movies
+
+        """
         __all_years = []
         for i, key in enumerate(self.movies):
             __all_years.append(key["year"])
@@ -369,66 +451,64 @@ class MovieLibrary:
         return __count_most_common.most_common()[0][0]
 
 
-"""
-production file json
+""" Absolute path where find file.json """
 
-file_json_path = f"{os.path.abspath("programming-principle-scritto/movies.json")}"
-"""
+file_json_to_deserialize = f"{
+    os.path.abspath("programming-principle-scritto/movies.json")
+    }"
 
-"""test file json"""
-file_json_to_deserialize = os.path.abspath(
-    "programming-principle-scritto/test.json"
-)
-""" define function that deserialize json """
-
-
-def jsonDeserializer(json_to_deserialize):
+def jsonDeserializer(json_to_deserialize) -> list:
+    """
+        Deserialize file json
+    
+    """
+    
     with open(json_to_deserialize, "r") as all_movies:
         return json.load(all_movies)
 
 
 movies_deserialized_list = jsonDeserializer(file_json_to_deserialize)
 
-
-library = MovieLibrary(
+movie_library = MovieLibrary(
     os.path.abspath("programming-principle-scritto/test.json"),
     movies_deserialized_list
 )
 
-# print(library.get_movies())
+# print(type(jsonDeserializer(file_json_to_deserialize)))
+# print(movie_library.get_movies())
 
-# library.add_movie("ciao","danny", 1500, ["speriamo", "bene"])
+# movie_library.add_movie("ciao","danny", 1500, ["speriamo", "bene"])
 
-# library.remove_movie("ciao")
+# movie_library.remove_movie("ciao")
 
-# library.update_movie("gatto","Top",1900, ["forse", "top"])
+# movie_library.update_movie("gatto","Top",1900, ["forse", "top"])
 
-# print(type(library.get_movie_titles()))
+# print(type(movie_library.get_movie_titles()))
 
-# print(library.get_movie_titles())
+# print(movie_library.get_movie_titles())
 
-# print(library.count_movies())
+# print(movie_library.count_movies())
 
-# print(library.get_movie_by_title("the matrix"))
+# print(movie_library.get_movie_by_title("the matrix"))
 
-# print(library.get_movie_by_title_substring("Lord"))
+# print(movie_library.get_movie_by_title_substring("Lord"))
 
-# print(library.get_movies_by_year(1994))
+# print(movie_library.get_movies_by_year(1994))
 
-# print(library.count_movies_by_director("Christopher NOLAN"))
+# print(movie_library.count_movies_by_director("Christopher NOLAN"))
 
-# print(library.get_movies_by_genre("DRama"))
+# print(movie_library.get_movies_by_genre("DRama"))
 
-# print(len(library.get_movies_by_genre("DRama")))
+# print(len(movie_library.get_movies_by_genre("DRama")))
 
-# print(library.get_oldest_movie_title())
+# print(movie_library.get_oldest_movie_title())
 
-# print(library.get_average_release_year())
+# print(movie_library.get_average_release_year())
 
-# print(type(library.get_average_release_year()))
+# print(type(movie_library.get_average_release_year()))
 
-# print(library.get_longest_title())
+# print(movie_library.get_longest_title())
 
-# print(library.get_titles_betweens_years(1990,1995))
+# print(movie_library.get_titles_betweens_years(1990,1995))
 
-# print(library.get_most_common_year())
+# print(movie_library.get_most_common_year())

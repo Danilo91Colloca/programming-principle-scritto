380ea486d405f9058182e631fd0e51c417675870
Danilo91Colloca <danilo.colloca91@gmail.com>
2025-01-17 15:12:32 +0100

    flake8 correction

--- movie_library.py
+++ movie_library.py
@@ -3,6 +3,8 @@ import collections
 import os
 
 """Define Movie library class"""
+
+
 class MovieLibrary:
 
     """
@@ -10,18 +12,19 @@ class MovieLibrary:
         and a collection of a movie that has title,director,year,genre
 
         Attributes:
-            json_file:  
+            json_file:
             movies: collection of film
     """
-    
-    def __init__(self, json_file: str, movies: list) :
+
+    def __init__(self, json_file: str, movies: list):
         self.json_file = json_file
         self.movies = movies
 
-        class FileNotFoundError(Exception):    
+        class FileNotFoundError(Exception):
             def __init__(self):
                 self.errorMsg = f"File not found: {json_file}"
                 super().__init__(self.errorMsg)
+
         def check_file_exist(file_json):
             if not os.path.exists(file_json):
                 raise FileNotFoundError
@@ -31,14 +34,14 @@ class MovieLibrary:
         except FileNotFoundError as e:
             print(e)
 
-    def get_movies(self) :
+    def get_movies(self):
         return self.movies
-    
+
     def __update_json_file(self, new_movie):
-        with open(self.json_file, "w", encoding="utf-8") as inside_movies :
+        with open(self.json_file, "w", encoding="utf-8") as inside_movies:
             return json.dump(new_movie, inside_movies, indent=4)
 
-    def add_movie(self, title: str, director: str, year: int, genres: list) :
+    def add_movie(self, title: str, director: str, year: int, genres: list):
         __data = {
             "title": title,
             "director": director,
@@ -47,42 +50,46 @@ class MovieLibrary:
         }
         self.movies.append(__data)
         self.__update_json_file(self.movies)
-    
-    class MovieNotFoundError(Exception) :
+
+    class MovieNotFoundError(Exception):
         def __init__(self):
             self.errorMsg = "Movie was not found"
             super().__init__(self.errorMsg)
 
-
-    def remove_movie(self, title) :
-        try :
+    def remove_movie(self, title):
+        try:
             __index = None
-            for index_item, key in enumerate(self.movies) :
-                """ 
-                casefold() consent to ignore 
-                case-sensitiveness 
+            for index_item, key in enumerate(self.movies):
+                """
+                casefold() consent to ignore
+                case-sensitiveness
                 considering all in lowercase
                 """
-                if key["title"].casefold() == title.casefold() :
+                if key["title"].casefold() == title.casefold():
                     __index = index_item
                     break
             if __index is not None:
                 self.movies.pop(__index)
                 self.__update_json_file(self.movies)
-            else :
+            else:
                 raise self.MovieNotFoundError
         except self.MovieNotFoundError as e:
             print(e)
 
-    def update_movie(self,title: str, director: str=None,year: int=None,genres: list=None):
-        try : 
+    def update_movie(
+            self, title: str,
+            director: str = None,
+            year: int = None,
+            genres: list = None
+            ):
+        try:
             __index = None
             for index_item, key in enumerate(self.movies):
                 if key["title"].casefold() == title.casefold():
                     __index = index_item
                     break
 
-            if __index is not None :
+            if __index is not None:
                 match (director, year, genres):
                     case (None, None, None):
                         pass
@@ -93,11 +100,11 @@ class MovieLibrary:
                     case (None, None, genres):
                         self.movies[__index].update({
                             "genres": genres
-                        })            
+                        })
                     case (None, year, None):
                         self.movies[__index].update({
                             "year": year
-                        })            
+                        })
                     case (director, None, genres):
                         self.movies[__index].update({
                             "director": director,
@@ -107,35 +114,34 @@ class MovieLibrary:
                         self.movies[__index].update({
                             "director": director,
                             "year": year
-                        })            
+                        })
                     case (None, year, genres):
                         self.movies[__index].update({
                             "year": year,
                             "genres": genres
-                        })            
-                    case (director,year,genres):
+                        })
+                    case (director, year, genres):
                         self.movies[__index].update({
-                            "director":director,
+                            "director": director,
                             "year": year,
                             "genres": genres
-                        })    
+                        })
                 self.__update_json_file(self.movies)
-            else :
+            else:
                 raise self.MovieNotFoundError
-                
-        except self.MovieNotFoundError as e :
-         print (e)
-                
+        except self.MovieNotFoundError as e:
+            print(e)
+
     def get_movie_titles(self):
-        __all_titles=[]
+        __all_titles = []
         for title in self.movies:
             __all_titles.append(title["title"])
-        
+
         return __all_titles
-    
+
     def count_movies(self):
         return len(self.movies)
-    
+
     def get_movie_by_title(self, title: str):
         __index = None
         for index_item, key in enumerate(self.movies):
@@ -144,14 +150,14 @@ class MovieLibrary:
                 break
         return self.movies[__index]
 
-    def get_movie_by_title_substring(self, substring:str):
-        __movies_by_substring=[]
+    def get_movie_by_title_substring(self, substring: str):
+        __movies_by_substring = []
         for index_item, key in enumerate(self.movies):
             if key["title"].find(substring) > -1:
                 __movies_by_substring.append(self.movies[index_item])
         return __movies_by_substring
 
-    def get_movies_by_year(self,year: int):
+    def get_movies_by_year(self, year: int):
         __index = None
         __all_movies_by_year = []
         for index_item, key in enumerate(self.movies):
@@ -160,13 +166,13 @@ class MovieLibrary:
                 __all_movies_by_year.append(self.movies[__index])
         return __all_movies_by_year
 
-    def count_movies_by_director(self,director: str):
+    def count_movies_by_director(self, director: str):
         __count_movies_by_director = 0
         for i, key in enumerate(self.movies):
             if key["director"].casefold() == director.casefold():
                 __count_movies_by_director += 1
         return __count_movies_by_director
-        
+
     def get_movies_by_genre(self, genre: str):
         __movies_by_genre = []
         for index_movie, key in enumerate(self.movies):
@@ -176,9 +182,12 @@ class MovieLibrary:
         return __movies_by_genre
 
     def get_oldest_movie_title(self):
-        __reorderd_movies_list = sorted(self.movies,key=lambda x: x['year'])
+        __reorderd_movies_list = sorted(
+            self.movies,
+            key=lambda x: x['year']
+        )
         return __reorderd_movies_list[0]
-        
+
     def get_average_release_year(self):
         __all_years_to_sum = []
         for index_item, key in enumerate(self.movies):
@@ -186,11 +195,15 @@ class MovieLibrary:
         return sum(__all_years_to_sum)/len(__all_years_to_sum)
 
     def get_longest_title(self):
-        __longest_title = sorted(self.movies, key=lambda k: len(k["title"]), reverse=True)
+        __longest_title = sorted(
+            self.movies,
+            key=lambda k: len(k["title"]),
+            reverse=True
+        )
         return __longest_title[0]["title"]
 
     def get_titles_betweens_years(self, start_year: int, end_year: int):
-        __title_list_list=[]
+        __title_list_list = []
         for i, key in enumerate(self.movies):
             if key["year"] >= start_year and key["year"] <= end_year:
                 __title_list_list.append(key["title"])
@@ -200,8 +213,9 @@ class MovieLibrary:
         __all_years = []
         for i, key in enumerate(self.movies):
             __all_years.append(key["year"])
-        __count_most_common = collections.Counter(__all_years)     
+        __count_most_common = collections.Counter(__all_years)
         return __count_most_common.most_common()[0][0]
+
 
 """
 production file json
@@ -210,32 +224,39 @@ file_json_path = f"{os.path.abspath("programming-principle-scritto/movies.json")
 """
 
 """test file json"""
-file_json_to_deserialize = os.path.abspath("programming-principle-scritto/test.json")  
-""" define function that deserialize json """       
+file_json_to_deserialize = os.path.abspath(
+    "programming-principle-scritto/test.json"
+)
+""" define function that deserialize json """
+
+
 def jsonDeserializer(json_to_deserialize):
     with open(json_to_deserialize, "r") as all_movies:
         return json.load(all_movies)
 
+
 movies_deserialized_list = jsonDeserializer(file_json_to_deserialize)
 
 
-library = MovieLibrary(os.path.abspath("programming-principle-scritto/test.json"), movies_deserialized_list)
-
-
-
-
-
+library = MovieLibrary(
+    os.path.abspath("programming-principle-scritto/test.json"),
+    movies_deserialized_list
+)
 
 # print(library.get_movies())
 
 # library.add_movie("ciao","danny", 1500, ["speriamo", "bene"])
+
 # library.remove_movie("ciao")
-library.update_movie("gatto","Top",1900, ["forse", "top"])
+
+# library.update_movie("gatto","Top",1900, ["forse", "top"])
 
 # print(type(library.get_movie_titles()))
+
 # print(library.get_movie_titles())
 
 # print(library.count_movies())
+
 # print(library.get_movie_by_title("the matrix"))
 
 # print(library.get_movie_by_title_substring("Lord"))
@@ -245,13 +266,17 @@ library.update_movie("gatto","Top",1900, ["forse", "top"])
 # print(library.count_movies_by_director("Christopher NOLAN"))
 
 # print(library.get_movies_by_genre("DRama"))
+
 # print(len(library.get_movies_by_genre("DRama")))
 
 # print(library.get_oldest_movie_title())
+
 # print(library.get_average_release_year())
+
 # print(type(library.get_average_release_year()))
 
 # print(library.get_longest_title())
-# print(library.get_titles_betweens_years(1990,1995))
-# print(library.get_most_common_year())
 
+# print(library.get_titles_betweens_years(1990,1995))
+
+# print(library.get_most_common_year())
